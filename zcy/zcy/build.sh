8015439de589f1cbcb117d98f76c39d62e8f1010
zcy <1935909821@qq.com>
2023-03-27 12:41:33 +0800

    master

--- zcy/zcy/build.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-npm install
-npm run build
-docker stop vue-builder
-docker rm vue-builder
-docker rmi vue-builder
-docker build -t vue-builder .
-docker run -p 9090:9090 --name vue-builder -d vue-builder
